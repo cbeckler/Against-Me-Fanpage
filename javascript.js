4e19b9bd0a11a27fde4df2446c08bbeb9aa224e1
Charlie Beckler <charlesabeckler@gmail.com>
2022-01-07 14:59:42 -0800

    added JS functionality

---
+++ javascript.js
@@ -0,0 +1,10 @@
+document.getElementById("bandBtn").addEventListener("click", function(){
+
+    document.getElementById("bandPic").style.transform = "scale(1.5)";
+
+});
+document.getElementById("bandBtn2").addEventListener("click", function(){
+
+    document.getElementById("bandPic").style.transform = "scale(1)";
+
+});
